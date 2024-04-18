1c32c1a5574e492e68342638f48d4af834e74c89
Gustav <gust4219@edu.nextkbh.dk>
2024-04-18 14:21:55 +0200

    Clear, color and undo

--- Tegneprogram.py
+++ Tegneprogram.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-#julie er her nu
+from tkinter import colorchooser
 
 class DrawingApp:
     def __init__(self, root):
@@ -25,15 +25,18 @@ class DrawingApp:
         self.prev_x = None
         self.prev_y = None
         self.size = 2  # Default size
+        self.color = "black"  # Default color
 
-        # Add buttons for changing tools
-        self.pencil_button = tk.Button(root, text="Pencil", command=self.use_pencil)
-        # Add buttons for changing tools and size
+        # History of drawn segments
+        self.segment_history = []
+        self.current_segment = []
+        
+        
+        # Add buttons for changing tools, size and undo
         self.pencil_button = tk.Button(
             root, text="Pencil", command=self.use_pencil)
         self.pencil_button.pack(side=tk.LEFT)
 
-        self.eraser_button = tk.Button(root, text="Eraser", command=self.use_eraser)
         self.eraser_button = tk.Button(
             root, text="Eraser", command=self.use_eraser)
         self.eraser_button.pack(side=tk.LEFT)
@@ -42,6 +45,14 @@ class DrawingApp:
             root, from_=1, to=10, orient=tk.HORIZONTAL, command=self.change_size)
         self.size_button.pack(side=tk.LEFT)
         self.size_button.set(self.size)  # Set default size
+
+        self.undo_button = tk.Button(
+            root, text="Undo", command=self.undo)
+        self.undo_button.pack(side=tk.LEFT)
+
+        self.color_button = tk.Button(
+            root, text="Color", command=self.choose_color)
+        self.color_button.pack(side=tk.LEFT)
 
         # Set default tool
         self.current_tool = "pencil"
@@ -64,14 +75,23 @@ class DrawingApp:
             if self.prev_x is not None and self.prev_y is not None:
                 if self.current_tool == "pencil":
                     # Draw using the pencil tool
-                    self.canvas.create_oval(
-                        x - self.size, y - self.size, x + self.size, y + self.size, fill="black")
+                    oval_id = self.canvas.create_oval(
+                        x - self.size, y - self.size, x + self.size, y + self.size, fill=self.color)
                     # Draw a line from the previous point to the current point
-                    self.canvas.create_line(
+                    line_id = self.canvas.create_line(
                         self.prev_x, self.prev_y, x, y, fill="black", width=self.size * 2)
+                    # Store segment info
+                    self.current_segment.append(oval_id)
+                    self.current_segment.append(line_id)
                 elif self.current_tool == "eraser":
-                    self.canvas.create_rectangle(
+                    # Erase using the eraser tool
+                    rect_id = self.canvas.create_rectangle(
                         x - self.size, y - self.size, x + self.size, y + self.size, fill="white", outline="")
+                    reline_id = self.canvas.create_line(
+                        self.prev_x, self.prev_y, x, y, fill="white", width=self.size * 2)
+                    # Store segment info
+                    self.current_segment.append(rect_id)
+                    self.current_segment.append(reline_id)
             # Update the previous point
             self.prev_x = x
             self.prev_y = y
@@ -81,7 +101,9 @@ class DrawingApp:
         self.drawing = False
         self.prev_x = None
         self.prev_y = None
-        self.history.append(self.canvas.find_all())
+        # Add the current segment to the segment history
+        self.segment_history.append(tuple(self.current_segment))
+        self.current_segment = []
           
 
 
@@ -94,11 +116,24 @@ class DrawingApp:
     def change_size(self, size):
         self.size = int(size)
     
+    def choose_color(self):
+        _, self.color = colorchooser.askcolor(title="Choose color")
+
+    #Fungere ikke
     def clear_sheet(self):
-        if self.history:
-            last_action = self.history.pop()
-            for item in last_action:
-                self.canvas.delete(item)
+        while(len(self.segment_history)>0):
+            last_segment = self.segment_history.pop()
+            for item_id in last_segment:
+                self.canvas.delete(item_id)
+
+    
+        
+    
+    def undo(self):
+        if self.segment_history:
+            last_segment = self.segment_history.pop()
+            for item_id in last_segment:
+                self.canvas.delete(item_id)
 
 
 if __name__ == "__main__":
