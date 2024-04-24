bffa0991c54348bd0adbaad52779fa629d2a0831
Gustav <gust4219@edu.nextkbh.dk>
2024-04-24 09:37:57 +0200

    Insert images and shapes

--- Tegneprogram.py
+++ Tegneprogram.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import colorchooser
+from tkinter import colorchooser, filedialog
+from PIL import Image, ImageDraw, ImageTk
 
 class DrawingApp:
     def __init__(self, root):
@@ -38,6 +39,14 @@ class DrawingApp:
             root, text="Eraser", command=self.use_eraser)
         self.eraser_button.pack(side=tk.LEFT)
 
+        self.square_button = tk.Button(
+            root, text="Square", command=self.use_square)
+        self.square_button.pack(side=tk.LEFT)
+
+        self.triangle_button = tk.Button(
+            root, text="Triangle", command=self.use_triangle)
+        self.triangle_button.pack(side=tk.LEFT)
+
         self.size_button = tk.Scale(
             root, from_=1, to=10, orient=tk.HORIZONTAL, command=self.change_size)
         self.size_button.pack(side=tk.LEFT)
@@ -50,6 +59,14 @@ class DrawingApp:
         self.color_button = tk.Button(
             root, text="Color", command=self.choose_color)
         self.color_button.pack(side=tk.LEFT)
+
+        self.insert_image_button = tk.Button(
+            root, text="Insert Image", command=self.insert_image)
+        self.insert_image_button.pack(side=tk.LEFT)
+
+        self.move_image_button = tk.Button(
+            root, text="Move Image", command=self.start_move_image)
+        self.move_image_button.pack(side=tk.LEFT)
 
         # Set default tool
         self.current_tool = "pencil"
@@ -87,9 +104,20 @@ class DrawingApp:
                         x - self.size, y - self.size, x + self.size, y + self.size, fill="white", outline="")
                     reline_id = self.canvas.create_line(
                         self.prev_x, self.prev_y, x, y, fill="white", width=self.size * 2)
+                elif self.current_tool == "square":
+                    # Draw a square
+                    square_id = self.canvas.create_rectangle(
+                        self.start_x, self.start_y, x, y, fill=self.color)
                     # Store segment info
-                    self.current_segment.append(rect_id)
-                    self.current_segment.append(reline_id)
+                    self.current_segment.append(square_id)
+                elif self.current_tool == "triangle":
+                    # Draw a triangle
+                    triangle_id = self.canvas.create_polygon(
+                        self.start_x, self.start_y, x, y, self.start_x - (x - self.start_x), y, fill=self.color)
+                    # Store segment info
+                    self.current_segment.append(triangle_id)
+                elif self.current_tool == "none":
+                    pass
             # Update the previous point
             self.prev_x = x
             self.prev_y = y
@@ -108,6 +136,12 @@ class DrawingApp:
 
     def use_eraser(self):
         self.current_tool = "eraser"
+    
+    def use_square(self):
+        self.current_tool = "square"
+
+    def use_triangle(self):
+        self.current_tool = "triangle"
 
     def change_size(self, size):
         self.size = int(size)
@@ -115,7 +149,39 @@ class DrawingApp:
     def choose_color(self):
         _, self.color = colorchooser.askcolor(title="Choose color")
 
-    #Fungere ikke
+    def insert_image(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            image = Image.open(file_path)
+            image.thumbnail((100, 100))  # Resize the image as needed
+            photo = ImageTk.PhotoImage(image)
+            self.current_image = photo
+            self.current_image_id = self.canvas.create_image(200, 200, image=photo)
+            self.canvas.image = photo  # Keep a reference to avoid garbage collection
+    
+    
+    def start_move_image(self):
+        self.current_tool = "none"
+        if self.current_image_id:
+            self.canvas.tag_bind(self.current_image_id, "<Button-1>", self.move_start)
+            self.canvas.tag_bind(self.current_image_id, "<B1-Motion>", self.move)
+            self.canvas.tag_bind(self.current_image_id, "<ButtonRelease-1>", self.move_stop)
+    
+    def move_start(self, event):
+        self.start_x = event.x
+        self.start_y = event.y
+    
+    def move(self, event):
+        if self.current_image_id:
+            dx = event.x - self.start_x
+            dy = event.y - self.start_y
+            self.canvas.move(self.current_image_id, dx, dy)
+            self.start_x = event.x
+            self.start_y = event.y
+    
+    def move_stop(self, event):
+        pass
+    
     def clear_sheet(self):
         while(len(self.segment_history)>0):
             last_segment = self.segment_history.pop()
